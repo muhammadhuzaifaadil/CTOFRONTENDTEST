8e591f4cd4672cec3e9a6387e8625c6fcb7a1929
muhammadhuzaifaadil <141272302+muhammadhuzaifaadil@users.noreply.github.com>
2025-10-29 04:26:18 -0700

    Add experimental allowedDevOrigins configuration

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  ,
+  experimental: {
+    allowedDevOrigins: ['*'],
+  } as any,
 };
 
 export default withNextIntl(nextConfig);
