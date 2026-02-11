95fb52631c1a8dc57d2e45d8ce0b2aafd62a016b
mhiplexsoft <muhammad.huzaifa@iplexsoft.com>
2026-02-11 15:12:08 +0500

    firebase

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -1,5 +1,22 @@
 // firebase.ts
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
+
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+// };
+
+// export const firebaseApp = initializeApp(firebaseConfig);
+
+
+
+// firebase.ts
+import { initializeApp, getApps, getApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -11,4 +28,7 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-export const firebaseApp = initializeApp(firebaseConfig);
+// Initialize Firebase only once
+export const firebaseApp = !getApps().length
+  ? initializeApp(firebaseConfig)
+  : getApp();
